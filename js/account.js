348706abde19acb4a86f6079361442cafe8aff89
Mark MacGillivray <mark@cottagelabs.com>
2016-09-10 19:06:45 +0100

    roll version

--- js/account.js
+++ js/account.js
@@ -1,27 +1,35 @@
 
+var foundkey;
+
 function save_auth(key) {
-  chrome.storage.local.set({
-    api_key: key
-  }, function() {
-    document.getElementById('plugin_messages').innerHTML = '<div class="alert alert-info"><p>Your API key ' + key + 'has been saved to your plugin.</p></div>';
-  });
+  if (key === undefined) key = foundkey;
+  if (key === undefined) {
+    console.log('Could not find key to save...');
+  } else {
+    chrome.storage.local.set({
+      api_key: key
+    }, function() {
+      document.getElementById('plugin_messages').innerHTML = '<div class="alert alert-info"><p>Your API key ' + key + 'has been saved to your plugin.</p></div>';
+    });
+  }
 }
 
 function do_auth() {
   document.getElementById('chromeinstall').style.display = 'none';
   chrome.storage.local.get({api_key : ''}, function(items) {
-    var key = document.getElementById('apikey').innerHTML;
+    foundkey = document.getElementById('apikey').innerHTML;
     if (items.api_key === '') {
-      if ( key !== "") {
-        save_auth(key);
+      if ( foundkey ) {
+        save_auth(foundkey);
       } else {
         document.getElementById('plugin_messages').innerHTML = '<div class="alert alert-info"><p>Your API key could not be found!</p></div>';
       }
-    } else if ( key !== items.api_key ) {
-      var html = '<div class="alert alert-info"><p>Your API key appears to have been changed. Do you want to save the new API key ' + key + ' into your plugin?</p>';
-      html += '<p><button class="btn" onclick="save_auth(' + key + ')">Yes, save the new API key to my plugin!</button></p>';
+    } else if ( foundkey !== items.api_key ) {
+      var html = '<div class="alert alert-info"><p>Your API key appears to have been changed.<br>Do you want to save the new API key ' + foundkey + ' into your plugin?</p>';
+      html += '<p><button id="update_key" class="btn btn-action" style="border:1px solid white;margin-top:5px;">Yes, save the new API key to my plugin!</button></p>';
       html += '</div>';
       document.getElementById('plugin_messages').innerHTML = html;
+      document.getElementById('update_key').onclick = save_auth;
     } else {
       document.getElementById('plugin_messages').innerHTML = '<div class="alert alert-info"><p>Your API key ' + items.api_key + ' is stored in your plugin.</p></div>';
     }
