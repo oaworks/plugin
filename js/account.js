77f93c55d5ceb99dd024aed78af68220acdf5d95
Mark MacGillivray <mark@cottagelabs.com>
2016-09-10 18:19:54 +0100

    roll version

--- js/account.js
+++ js/account.js
@@ -1,26 +1,21 @@
 
 function do_auth() {
-  document.getElementsByClassName('dl').style.display = 'none';
+  $('.dl').hide();
   chrome.storage.local.get({api_key : ''}, function(items) {
     if (items.api_key === '') {
       var key = document.getElementById('apikey').value;
       if ( key !== "") {
         chrome.storage.local.set({
-          api_key: api_key
+          api_key: key
         }, function() {
-          document.getElementById('plugin_messages').textContent = 'Your API key has been saved to your plugin.';
+          document.getElementById('plugin_messages').textContent = 'Your API key ' + key + 'has been saved to your plugin.';
         });
       } else {
-        document.getElementById('plugin_messages').textContent = 'Your API key could not be found!';        
+        document.getElementById('plugin_messages').textContent = 'Your API key could not be found!';
       }
     } else {
-      document.getElementById('plugin_messages').textContent = 'Your API key is already stored in your plugin.';
+      document.getElementById('plugin_messages').textContent = 'Your API key ' + items.api_key + 'is stored in your plugin.';
     }
   });
 
 }
-
-setTimeout(function() {
-  do_auth();
-}, 3000);
-
