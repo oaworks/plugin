74b1df78f4ad6c8ed1394ceda1aff72a554787d4
Mark MacGillivray <mark@cottagelabs.com>
2016-10-23 20:12:50 +0100

    bugfixes

--- js/main.js
+++ js/main.js
@@ -26,16 +26,21 @@ function handleAvailabilityResponse(response) {
     }
   } else if (response.data.requests.length > 0) {
     for (var requests_entry of response.data.requests) {
-      if (requests_entry.usupport || requests_entry.ucreated) {
-        document.getElementById('icon'+requests_entry.type).setAttribute('href',oab.site_address+'/request/'+requests_entry._id);
+      if (requests_entry.usupport) {
+        document.getElementById('icon'+requests_entry.type).setAttribute('data-action','supported');
+        document.getElementById('submit').setAttribute('data-action','supported');
+      } else if (requests_entry.ucreated) {
+        document.getElementById('icon'+requests_entry.type).setAttribute('data-action','created');
+        document.getElementById('submit').setAttribute('data-action','created');        
       } else {
         document.getElementById('icon'+requests_entry.type).setAttribute('data-action','support');
         document.getElementById('submit').setAttribute('data-action','support');
-        document.getElementById('submit').setAttribute('data-support',requests_entry._id);
-        var rnd = document.getElementById('icon'+requests_entry.type).innerHTML;
-        rnd = rnd.replace('Unavailable','Support '+requests_entry.type);
-        document.getElementById('icon'+requests_entry.type).innerHTML = rnd;
       }
+document.getElementById('icon'+requests_entry.type).setAttribute('href',oab.site_address+'/request/'+requests_entry._id);
+      document.getElementById('submit').setAttribute('data-support',requests_entry._id);
+      var rnd = document.getElementById('icon'+requests_entry.type).innerHTML;
+      rnd = rnd.replace('Unavailable','Support '+requests_entry.type);
+      document.getElementById('icon'+requests_entry.type).innerHTML = rnd;
     }
   } else if (response.data.accepts.length > 0) {
     for (var accepts_entry of response.data.accepts) {
@@ -137,22 +142,36 @@ for ( var n in needs ) {
   needs[n].onclick = function(e) {
     var href = e.target.getAttribute('href');
     if (!href) href = e.target.parentNode.getAttribute('href');
+    oab.displayMessage('');
     if ( href === '#' && api_key ) {
       e.preventDefault();
       var action = e.target.getAttribute('data-action');
       if (!action) action = e.target.parentNode.getAttribute('data-action');
-      var type = e.target.getAttribute('data-type');
-      if (!type) type = e.target.parentNode.getAttribute('data-type');
-      var ask = action === 'support' ? 'There is an open request for this ' + type + '. Add your support. ' : 'Create a new ' + type + ' request. ';
-      ask += 'How would getting access to this ' + type + ' help you? This message will be sent to the author.';
-      document.getElementById('story').setAttribute('placeholder',ask);
-      document.getElementById('submit').setAttribute('data-type',type);
-      if ( action === 'support' ) {
-        document.getElementById('submit').innerHTML = document.getElementById('submit').innerHTML.replace('create','support');
+      if (action === 'created' || action === 'supported') {
+        var rid = document.getElementById('submit').getAttribute('data-support');
+        var u = oab.site_address + 'request/' + rid;
+        var dm = '<p>You already ' + action + ' a request for this.<br>';
+        dm += '<a id="goto_request" href="' + u + '">' + u + '</a></p>';
+        if (chrome && chrome.tabs) {
+          document.getElementById('goto_request').onclick = function () {
+            chrome.tabs.create({'url': oab.site_address + 'request/' + rid});
+          };          
+        }
+        oab.displayMessage(dm);
       } else {
-        document.getElementById('submit').innerHTML = document.getElementById('submit').innerHTML.replace('support','create');
+        var type = e.target.getAttribute('data-type');
+        if (!type) type = e.target.parentNode.getAttribute('data-type');
+        var ask = action === 'support' ? 'There is an open request for this ' + type + '. Add your support. ' : 'Create a new ' + type + ' request. ';
+        ask += 'How would getting access to this ' + type + ' help you? This message will be sent to the author.';
+        document.getElementById('story').setAttribute('placeholder',ask);
+        document.getElementById('submit').setAttribute('data-type',type);
+        if ( action === 'support' ) {
+          document.getElementById('submit').innerHTML = document.getElementById('submit').innerHTML.replace('create','support');
+        } else {
+          document.getElementById('submit').innerHTML = document.getElementById('submit').innerHTML.replace('support','create');
+        }
+        document.getElementById('story_div').className = document.getElementById('story_div').className.replace('collapse','').replace('  ',' ');
       }
-      document.getElementById('story_div').className = document.getElementById('story_div').className.replace('collapse','').replace('  ',' ');
     } else if (chrome && chrome.tabs && api_key) {
       chrome.tabs.create({'url': e.target.getAttribute('href')});
     } else {
@@ -213,7 +232,7 @@ document.getElementById('story').onkeyup = function () {
     document.getElementById('submit').style.backgroundColor = '#99ff99'; 
   }
   if (length > 20) {
-    document.getElementById('submit').innerHTML('<span id="counter"></span>... Click now to submit your<br>story and create your request');
+    document.getElementById('submit').innerHTML = '<span id="counter"></span>... Click now to submit your<br>story and create your request';
     document.getElementById('submit').style.backgroundColor = '#99ff99'; 
   }
   document.getElementById('counter').innerHTML = left;
