116a49881b1a9efd954182bd6d208699894e66fd
Mark MacGillivray <mark@cottagelabs.com>
2017-08-31 00:40:44 +0100

    trying icon animation

--- js/background.js
+++ js/background.js
@@ -1,4 +1,7 @@
-/*function updateIcon() {
+// add to manifest to enable background script if desired later:
+// "background" : { "scripts" : ["background.js"] },
+  
+function updateIcon() {
   chrome.browserAction.setIcon({path:"../img/spin_orange.png"});
 }
-chrome.browserAction.onClicked.addListener(updateIcon);*/
+chrome.browserAction.onClicked.addListener(updateIcon);
